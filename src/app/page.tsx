e481bab065dd0736c99cdac2c65bf6bfc1a89c80
hackthonprojecttext <Khanzoni361@gmail.com>
2024-12-10 00:48:17 +0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,132 @@
 import Image from "next/image";
+import Link from "next/link";
+import { IoBag } from "react-icons/io5";
+import { TiTick } from "react-icons/ti";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="bg-black w-[100%] h-[100vh]">
+        <div className="w-full bg-black text-white">
+          <h1 className="pt-6 text-center text-[23px] text-[#FF9F0D]">
+            Food<span className="text-white">truck</span>
+          </h1>
         </div>
+
+        <div className="w-[90%] h-[41px] mt-2 flex items-center justify-evenly bg-black">
+          <div className="flex justify-center items-center gap-[32px] text-white">
+            <Link href={"#"}>Home</Link>
+            <Link href={"#"}>Menu</Link>
+            <Link href={"#"}>Blog</Link>
+            <Link href={"#"}>Pages</Link>
+            <Link href={"#"}>About</Link>
+            <Link href={"#"}>Shop</Link>
+            <Link href={"#"}>Contact</Link>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              className="h-[48px] border-2 border-[#FF9F0D] bg-black  rounded-3xl w-[300px] px-7"
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Seacrh..."
+            />
+            <div className="text-[20px] text-white"><IoBag  /></div>
+          </div>
+        </div>
+
+        <div className="w-[100%] flex">
+          <div className="w-[50%] h-[100%]">
+            <Image className="mt-[110px] pl-[120px]" src={"/myimage/quick.png"} alt="" width={400} height={400} />
+            <h1 className="w-[90%] h-[full] text-white text-[60px] mt-3 font-bold pl-[120px]"><span className="text-[#FF9F0D]">Th</span>e Art of Speed food Quality</h1>
+            <p className="w-[80%] h-[full] pl-[120px] text-white mt-2 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam vero delectus voluptates autem add.</p>
+            <button className="w-[200px] ml-[120px] h-[50px] rounded-full text-white mt-3 bg-[#FF9F0D] font-bold ">See Menu</button>
+          </div>
+          <div className="w-[50%] h-[90%]">
+            <Image className="ml-[20px] mt-[25px]" src={"/myimage/food.png"} alt="Food Image" width={600} height={600} />
+
+          </div>
+        </div>
+
+        <div className="w-[100%] h-[100vh] bg-black flex ">
+          <div className="w-[50%] h-full">
+            <Image className="mt-[40px] ml-[120px] " src={"/myimage/aboutus.png"} alt="About us" width={100} height={100} />
+            <h1 className="text-[55px] font-bold text-white ml-[120px]"><span className="text-[#FF9F0D]">We</span> Create the best foody product</h1>
+            <p className="w-[73%] ml-[120px] text-white mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque diam pellentesque bibendum non dui volutpat fringilla bibendum. Urna, elit augue urna, vitae feugiat pretium donec id elementum. Ultrices mattis sed vitae mus risus. Lacus nisi, et ac dapibus sit eu velit in consequat.</p>
+            <h3 className="flex items-center mt-2 text-white text-[15px]"><div className="ml-[120px] text-[22px] text-white"><TiTick  /></div>Lacus nisi, et ac dapibus sit eu velit in consequat.</h3>
+
+            <h3 className="flex items-center mt-2 text-white text-[15px]"><div className="ml-[120px] text-[22px] text-white"><TiTick  /></div>Quisque diam pellentesque bibendum non dui volutpat fringilla </h3>
+
+            <h3 className="flex items-center mt-2 text-white text-[15px]"><div className="ml-[120px] text-[22px] text-white"><TiTick  /></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <button className="w-[200px] ml-[120px] h-[50px] rounded-full text-white mt-4 bg-[#FF9F0D] font-bold ">Read More</button>
+          </div>
+
+          <div className="w-[50%] h-full   mt-[50px] ">
+            <div>
+              <Image className="m-auto" src={"/myimage/unsplash.png"} alt="" width={700} height={400} />
+            </div>
+              <div className="flex items-center justify-evenly mt-4">
+                <Image className="" src={"/myimage/kabab.png"} alt="" width={330} height={400} />
+                <Image className="" src={"/myimage/burger.png"} alt="" width={330} height={400} />
+              </div>
+          </div>
+        </div>
+
+        <div className="w-[100%] h-[70vh] bg-black ">
+          <Image className="m-auto pt-6" src={"/myimage/foodcategory.png"} alt="" width={200} height={200} />
+          <h1 className="text-white text-center text-[40px]"><span className="text-[#FF9F0D]">Ch</span>oose Food Iteams</h1>
+          <div className="flex justify-evenly mt-6">     
+          <div><Image src={"/myimage/icecreame.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/beefburger.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/saled.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/cake.png"} alt="" width={200} height={200} /></div>
+          </div>
+        </div>
+
+        <div className="w-[full] h-[90vh] bg-black">
+        <div><Image className="w-[70%] ml-[120px] h-[70%]" src={"/myimage/fullimage.png"} alt="" width={400} height={400} /></div>
+        </div>
+
+        <div className="w-[full] h-[40vh] bg-black flex justify-between ">
+        <div><Image className=" mx-[120px]" src={"/myimage/Chef.png"} alt="" width={180} height={180} /></div>
+        <div><Image src={"/myimage/Iteam.png"} alt="" width={135} height={135} /></div>
+        <div><Image src={"/myimage/spoon.png"} alt="" width={210} height={210} /></div>
+        <div><Image className=" mr-[120px]" src={"/myimage/pizza.png"} alt="" width={180} height={180} /></div>
+        </div>
+
+        <div className="w-[100%] h-[70vh] bg-black">
+        <div><Image className=" m-auto" src={"/myimage/Chefs.png"} alt="" width={50} height={50} /></div>
+        <h1 className="text-white text-[30px] text-center"><span className="text-[#FF9F0D]">Me</span>et Our Chef</h1>
+        <div className="flex justify-evenly mt-6">     
+          <div><Image src={"/myimage/estwood.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/Rectangle.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/william.png"} alt="" width={200} height={200} /></div>
+            <div><Image src={"/myimage/readroad.png"} alt="" width={200} height={200} /></div>
+          </div>
+          <div className="flex items-center justify-center mt-3">
+          <button className="w-[200px] h-[50px] bg-black border-2 border-[#FF9F0D] rounded-full  text-white mt-4 font-bold ">See More</button>
+          </div>  
+        </div>
+
+        <div className="w-[100%] h-[30vh] bg-black pt-6">
+          <Image className="ml-[110px]" src={"/myimage/Testimonials.png"} alt="" width={100} height={100} />
+          <h1 className="text-white text-[30px] ml-[110px] mt-1">What our client are saying</h1>
+        </div>
+
+        <div className="w-[100%] h-[50vh] bg-black">
+          <Image className="w-[100%] m-auto h-[50vh]" src={"/myimage/Footer.png"} alt="" width={1000} height={400} />
+        </div>
+
+
+
+        
+
+
+
+
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
+
+
